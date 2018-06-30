bf6e63c1f18e51b96e282f92e7613dd64d36cb07
Dakota Sitton <dakotasitton@gmail.com>
2018-06-30 09:37:42 -0500

    Add project files.

---
+++ graderanker/GradeBook.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace graderanker
+{
+    class GradeBook
+    {
+
+        public GradeBook()
+        {
+            grades = new List<float>();  
+        }
+
+        public GradeStatistics ComputeStatistics()
+        {
+            GradeStatistics stats = new GradeStatistics();
+            stats.HighestGrade = 0;
+            stats.LowestGrade = 100;
+
+            float sum = 0;
+            foreach (float grade in grades)
+            {
+                stats.HighestGrade = Math.Max(grade, stats.HighestGrade);
+                stats.LowestGrade = Math.Min(grade, stats.HighestGrade);
+                sum += grade;
+            }
+            stats.AverageGrade = sum / grades.Count;
+            return stats;
+        }
+
+        public void AddGrade(float grade)
+        {
+            grades.Add(grade);
+        }
+
+        List<float> grades;
+        internal bool HighestGrade;
+    }
+}
---
+++ graderanker/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace graderanker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GradeBook book = new GradeBook();
+            book.AddGrade(91);
+            book.AddGrade(87.5f);
+            book.AddGrade(56);
+            book.AddGrade(24);
+            GradeStatistics stats = book.ComputeStatistics();
+
+            GradeBook book2 = book;
+            book2.AddGrade(67);
+
+            Console.WriteLine(stats.HighestGrade);
+            Console.WriteLine(stats.LowestGrade);
+            Console.WriteLine(stats.AverageGrade);
+
+        }
+    }
+}
